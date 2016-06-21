5ff0eb603d25a09a0f25922183a8d8ca767e024c
vkhristenko <vdkhristenko1991@gmail.com>
2016-06-21 21:22:08 +0200

    updates

--- NtupleMaking/plugins/QIE10Maker.cc
+++ NtupleMaking/plugins/QIE10Maker.cc
@@ -104,7 +104,7 @@ QIE10Maker::analyze(const edm::Event& e, const edm::EventSetup& es)
 	for (uint32_t i=0; i<cqie10->size(); i++)
 	{
 		QIE10DataFrame frame = static_cast<QIE10DataFrame>((*cqie10)[i]); 
-		QIE10Frame df(HcalDetId(frame.detid()));
+		QIE10Frame df(HcalDetId(frame.detid()).rawId());
 		for (int j=0; j<frame.samples(); j++)
 		{
 			df._adc[j] = frame[j].adc();
