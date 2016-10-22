e32194e59a6f1c4dad1ccf44dcecae74dcbb033f
vkhristenko <vdkhristenko1991@gmail.com>
2016-10-22 13:15:27 +0200

    fixing the crab template not to transfer the logs

--- NtupleMaking/test/templates/crab_template.py
+++ NtupleMaking/test/templates/crab_template.py
@@ -4,7 +4,7 @@ config = config()
 config.General.requestName = 'REQUESTNAME'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
-config.General.transferLogs = True
+config.General.transferLogs = False
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'cfgname'
